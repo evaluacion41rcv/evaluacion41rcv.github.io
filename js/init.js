b78f3c8c80635525cbdb4fd8d018274e118732ff
evaluacion41rcv <evaluacion41rcv@gmail.com>
2023-04-09 01:41:19 -0600

    cambios

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBqB_lbvJxzYimBHV7DZFhkoiEEX_nCXU8",
+    authDomain: "evaluacion41vrc1-65cfa.firebaseapp.com",
+    projectId: "evaluacion41vrc1-65cfa",
+    storageBucket: "evaluacion41vrc1-65cfa.appspot.com",
+    messagingSenderId: "1068121951081",
+    appId: "1:1068121951081:web:f4c0f3be26e3e0901c8bf8",
+    measurementId: "G-7VLTLN2G1Y"
+});
